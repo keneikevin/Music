0f9a1a1feb162274b228b65c850ee1cce1ac12b1
keneikevin <kevinkenei9@gmail.com>
2021-02-13 11:53:42 +0300

    Song ViewModel

---
+++ app/src/main/java/com/kenei/music/ui/viewmodel/SongViewModel.kt
@@ -0,0 +1,41 @@
+package com.kenei.music.ui.viewmodel
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kenei.music.exoplayer.MusicService
+import com.kenei.music.exoplayer.MusicServiceConnection
+import com.kenei.music.exoplayer.currentPlayBackPosition
+import com.kenei.music.other.Constants.UPDATE_PLAYER_POSITION_INTERVAL
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SongViewModel @ViewModelInject constructor(
+    musicServiceConnection: MusicServiceConnection
+): ViewModel() {
+    private val playbackState = musicServiceConnection.playbackState
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration:LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition:LiveData<Long> = _curPlayerPosition
+
+    init {
+        updateCurrentPlayerPosition()
+    }
+
+    private fun updateCurrentPlayerPosition(){
+        viewModelScope.launch {
+            while (true){
+            val pos = playbackState.value?.currentPlayBackPosition
+                if (curPlayerPosition.value != (pos)){
+                 _curPlayerPosition.postValue(pos)
+                    _curSongDuration.postValue(MusicService.curSongDuration)
+                }
+                delay(UPDATE_PLAYER_POSITION_INTERVAL)
+            }
+        }
+    }
+}
